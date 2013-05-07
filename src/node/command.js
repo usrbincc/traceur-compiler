70a79fdb2bd56246e5bd47da478f770c4724c76a
usrbincc <usrbincc@yahoo.com>
2013-05-07 13:12:38 -0400

    Remove all commander-related code.

--- src/node/command.js
+++ src/node/command.js
@@ -19,14 +19,6 @@ var sprintf = require('./fmt-util.js').sprintf;
 var path = require('path');
 var flags;
 var cmdName = path.basename(process.argv[1]);
-try {
-  flags = new (require('commander').Command)(cmdName);
-} catch (ex) {
-  console.error('Commander.js is required for this to work. To install it ' +
-                'run:\n\n  npm install commander\n');
-  process.exit(1);
-}
-flags.setMaxListeners(100);
 
 var traceur = require('./traceur.js');
 
@@ -155,102 +147,6 @@ while (g.getopt(argv)) {
 }
 console.log(f);
 process.exit(0);
-
-flags.option('--sourcemap', 'Generate source maps');
-flags.on('sourcemap', function() {
-  flags.sourceMaps = traceur.options.sourceMaps = true;
-});
-
-flags.option('--longhelp', 'Show all known options');
-flags.on('longhelp', function() {
-  flags.help();
-  process.exit();
-});
-
-traceur.options.addOptions(flags);
-
-flags.usage('[options] [files]');
-
-// Override commander.js's optionHelp to filter out the Traceur feature flags
-// from showing up in the help message.
-var optionHelp = flags.optionHelp;
-flags.optionHelp = function() {
-  if (!flags.longhelp) {
-    this.options = this.options.filter(function(command) {
-      var dashedName = command.long.slice(2);
-      return traceur.options.filterOption(dashedName);
-    });
-  }
-  return optionHelp.call(this);
-}
-
-/**
- * HACK: Process arguments so that in interpret mode, commander.js only parses
- * the flags, without the file name and anything past that. If an invalid flag
- * is encountered, commander.js error reporting is emulated instead.
- * @param {Array.<string>} argv
- * @return {Array.<string>}
- */
-function processArguments(argv) {
-  // Preserve the original.
-  argv = argv.slice();
-
-  var interpretMode = true;
-  for (var i = 2; i < argv.length; i++) {
-    var arg = argv[i], index;
-    if (arg === '--')
-      break;
-
-    // Normalize flags in-place.
-    if (arg.length > 2 && arg[0] === '-' && arg[1] !== '-') {
-      // TODO: Is this needed at all for traceur?
-      arg = arg.slice(1).split('').map(function(flag) {
-        return '-' + flag;
-      });
-      // Insert the normalized flags in argv.
-      argv.splice.apply(argv, [i, 1].concat(arg));
-      // Grab the first normalized flag and process it as usual.
-      arg = argv[i];
-    } else if (/^--/.test(arg) && (index = arg.indexOf('=')) !== -1) {
-      // Insert the flag argument in argv.
-      argv.splice(i + 1, 0, arg.slice(index + 1));
-      // Replace the flag with the stripped version and process it as usual.
-      arg = argv[i] = arg.slice(0, index);
-    }
-
-    var option = flags.optionFor(arg);
-    if (option) {
-      if (arg === '--out')
-        interpretMode = false;
-
-      if (option.required)
-        i++;
-      else if (option.optional) {
-        arg = argv[i + 1];
-        if (arg && arg[0] !== '-')
-          i++;
-      }
-    } else if (arg[0] === '-') {
-      // HACK: Because commander.js has a flexible policy, this is the only
-      // reliable way of reporting invalid flags to the user, and it's limited
-      // to the first invalid flag encountered.
-      console.log('\n  error: unknown option `%s\'\n', arg);
-      process.exit(1);
-    } else if (interpretMode) {
-      // Add a hint to stop commander.js from parsing following arguments.
-      // Note that this means that --out must come before any unknown flag as
-      // well as before any filename for it to be used as the out flag.
-      argv.splice(i, 0, '--');
-      // Save traceur flags for interpret.js.
-      argv.flags = argv.slice(2, i);
-      break;
-    }
-  }
-  return argv;
-}
-
-var argv = processArguments(process.argv);
-flags.parse(argv);
 
 var includes = flags.args;
 
